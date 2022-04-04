0d310b7863f972b745bb5ccd78afea31f28bc371
Katarina Komad <komad.katarina@gmail.com>
2022-04-04 18:41:19 +0200

    Project initialization

---
+++ tim018/src/main/java/mrsa/tim018/Tim018Application.java
@@ -0,0 +1,13 @@
+package mrsa.tim018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim018Application.class, args);
+	}
+
+}
---
+++ tim018/src/test/java/mrsa/tim018/Tim018ApplicationTests.java
@@ -0,0 +1,13 @@
+package mrsa.tim018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tim018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
