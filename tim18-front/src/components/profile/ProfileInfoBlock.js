282ce59dc9dba59df137d066425ee912ed9043dd
mmmajder <ajder.milan2000@gmail.com>
2022-04-15 10:52:16 +0200

    created hooks for profile of instructor

--- tim18-front/src/components/profile/ProfileInfoBlock.js
+++ tim18-front/src/components/profile/ProfileInfoBlock.js
@@ -2,37 +2,50 @@ import '../../assets/styles/profile.css';
 import MarkStars from '../MarkStars';
 import ProfileInfo from './ProfileInfo';
 import ProfileBusinessInfo from './ProfileBusinessInfo';
-import {useCallback, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
+import axios from 'axios';
 
-// const fetchUser = async(id) => {
-//     const res = await fetch()
-//     const data = await res.json()
-
-//     return data
-// }
-
-const fetchUser = async(id) => {
-    // GET request using fetch with async/await
-    const response = await fetch(`http://localhost:8082/users/${id}`);
-    return await response.json();
-}
+export default function ProfileInfoBlock({id}){
+    const [user, setUser] = useState();
 
 
-export default async function ProfileInfoBlock({id}){
+    useEffect(() => {
+        const fetchData = async() => {
+            
+            const data = await fetch(`http://localhost:8000/users/${id}`)
+            const json = await data.json()
+            setUser(json)
+        }
+        fetchData();
+
+    })
+
+    const user1 = {
+        firstName: "Zema",
+        lastName: "Moreplovac",
+        address: "Topolska 18",
+        city: "Backa Topola",
+        state: "Srbija",
+        phoneNumber: "064 1231231",
+        userType: "FISHINGINSTRUCTOR",
+        birthDate:  "23.11.2000.",
+        profilePic: "../assets/images/blue_profile_pic.jpg",
+        mark: 4.2
+      }
+   // setUser(user1)
 
     const profilePic = require('../../assets/images/blue_profile_pic.jpg')
     console.log(id)
-    const user = await fetchUser(id)
-
-    return <div className="borderedBlock" align="center">
+    console.log(user)
+    return (<div className="borderedBlock" align="center">
                 <img src={profilePic} className="profilePicture rounded-circle" ></img>
                 <ProfileInfo infoClass="profileNameLastname" text={user.firstName + " " + user.lastName}/>
                 <MarkStars mark={user.mark} />
                 <ProfileInfo infoClass="profileOtherInfo" text={user.city + ", " + user.state }/>
                 <ProfileInfo infoClass="profileOtherInfo" text={user.dateBirth}/>
                 <hr className="solidDivider"/>
-                <ProfileBusinessInfo assetsName={getAssetName(user.userType)} assetsNum="5" rentsName="RENTS" rentsNum="7" reviewsNum="3"/>
-            </div>
+                <ProfileBusinessInfo assetsName={ getAssetName(user.userType)} assetsNum="5" rentsName="RENTS" rentsNum="7" reviewsNum="3"/>
+            </div>)
 }
 
 const getAssetName = (userType) => {
