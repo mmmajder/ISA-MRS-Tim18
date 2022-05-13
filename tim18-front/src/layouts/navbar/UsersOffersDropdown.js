4e353bab63854ad143e3f9fba100d88592486858
Aksont <aastanivuk@gmail.com>
2022-05-13 21:55:47 +0200

    usersNavbar fix

--- tim18-front/src/layouts/navbar/UsersOffersDropdown.js
+++ tim18-front/src/layouts/navbar/UsersOffersDropdown.js
@@ -4,36 +4,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faHouseChimney, faPlus, faBook, faChartLine, faFishFins, faShip, faAnchor} from '@fortawesome/free-solid-svg-icons'
 
 const UsersOffersDropdown = ({userType}) => {
-    let retData = "";
+    let assetsName;
+    let assetsIcon;
+    
     switch (userType) {
         case "FishingInstructor":
-            retData=(<NavDropdown title={<span><FontAwesomeIcon icon={faFishFins} />Adventures </span>}> 
-                <NavDropdown.Item href="/resorts"><FontAwesomeIcon icon={faFishFins} /> My adventures</NavDropdown.Item> 
-                <NavDropdown.Item href="/createResort"><FontAwesomeIcon icon={faPlus} /> Add new adventures</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faBook} /> Adventures history</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faChartLine} /> Reports</NavDropdown.Item>
-            </NavDropdown>)
+            assetsName = "Adventure";
+            assetsIcon = faFishFins;
             break;
         case "ResortRenter":
-            retData=(<NavDropdown title={<span><FontAwesomeIcon icon={faHouseChimney} />Resorts </span>}> 
-                <NavDropdown.Item href="/resorts"><FontAwesomeIcon icon={faHouseChimney} /> My resorts</NavDropdown.Item> 
-                <NavDropdown.Item href="/createResort"><FontAwesomeIcon icon={faPlus} /> Add new resorts</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faBook} /> Resorts history</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faChartLine} /> Reports</NavDropdown.Item>
-            </NavDropdown>)
+            assetsName = "Resort";
+            assetsIcon = faHouseChimney;
             break;
         case "BoatRenter":
-            retData=(<NavDropdown title={<span><FontAwesomeIcon icon={faAnchor} />Boats </span>}> 
-                <NavDropdown.Item href="/resorts"><FontAwesomeIcon icon={faShip} /> My boats</NavDropdown.Item> 
-                <NavDropdown.Item href="/createResort"><FontAwesomeIcon icon={faPlus} /> Add new boats</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faBook} /> Boats history</NavDropdown.Item>
-                <NavDropdown.Item href="#"><FontAwesomeIcon icon={faChartLine} /> Reports</NavDropdown.Item>
-            </NavDropdown>)
+            assetsName = "Boat";
+            assetsIcon = faShip;
             break;
         default:
             break;
     }
-  return retData;
+
+  return (
+    <NavDropdown title={<span><FontAwesomeIcon icon={assetsIcon} /> {assetsName}s</span>}> 
+        <NavDropdown.Item href="/resorts"><FontAwesomeIcon icon={assetsIcon} /> My {assetsName}s</NavDropdown.Item> 
+        <NavDropdown.Item href="/createResort"><FontAwesomeIcon icon={faPlus} /> Add new {assetsName}</NavDropdown.Item>
+        <NavDropdown.Item href="#"><FontAwesomeIcon icon={faBook} /> {assetsName}s history</NavDropdown.Item>
+        <NavDropdown.Item href="#"><FontAwesomeIcon icon={faChartLine} /> Reports</NavDropdown.Item>
+    </NavDropdown>
+    );
 }
 
 export default UsersOffersDropdown
