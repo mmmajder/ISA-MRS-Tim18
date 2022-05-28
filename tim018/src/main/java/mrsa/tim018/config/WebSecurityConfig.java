5ccb5f5f25be63f97f51ef75ef92e9fc11d7e01d
KatarinaKomad <komad.katarina@gmail.com>
2022-05-28 20:49:30 +0200

    #17.1.1 Authentification config for photos and prices

--- tim018/src/main/java/mrsa/tim018/config/WebSecurityConfig.java
+++ tim018/src/main/java/mrsa/tim018/config/WebSecurityConfig.java
@@ -85,6 +85,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 								.antMatchers("/assets").permitAll()
 								.antMatchers("/assets/{id}").permitAll()
 								.antMatchers("/users/verify/{code}").permitAll()
+								.antMatchers("/photos/assetPhotoIds/{id}").permitAll()
+								.antMatchers("/photos/{id}").permitAll()
+								.antMatchers("/prices/today/{assetId}").permitAll()
+								
+								
 								
 			// ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
 			// koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
